39c369e5a6bc4ae573ac669f5c50b231a9dea790
Aline Korshunova <aline.korshunova00@gmail.com>
2021-04-06 21:13:28 +0300

    Connection.java is a Message exchange send() - serializes the message message to out, receive() - deserializes data from in, getRemoteSocketAddress() - returns the remote address of the socket connection, close() - closes all resources.

--- src/Connection.java
+++ src/Connection.java
@@ -1,3 +1,38 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 //Класс соединения между клиентом и сервером
-public class Connection {
+public class Connection implements Closeable{
+    private final Socket socket;
+    private final ObjectInputStream in;
+    private final ObjectOutputStream out;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void send(Message message) throws IOException{ //сериализует сообщение message в out
+        synchronized (out){
+            out.writeObject(message);
+        }
+    }
+
+    public Message receive() throws IOException, ClassNotFoundException{ //десериализует данные из in
+        synchronized (in){
+            return (Message)in.readObject();
+        }
+    }
+
+    public SocketAddress getRemoteSocketAddress(){ //возвращает удаленный адрес сокетного соединения
+        return socket.getRemoteSocketAddress();
+    }
+
+    public void close() throws IOException{ //закрывает все ресурсы
+        socket.close();
+        in.close();
+        out.close();
+    }
 }
