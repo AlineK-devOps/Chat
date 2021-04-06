4da0d0fd5456ba27681536e3d730b0ac6e9a7243
Aline Korshunova <aline.korshunova00@gmail.com>
2021-04-06 19:32:40 +0300

    Implemented the class ConsoleHelper.java Added methods: writeMessage(), readString(), readInt().

--- src/ConsoleHelper.java
+++ src/ConsoleHelper.java
@@ -1,3 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //Вспомогательный класс, для чтения и записи в консоль
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){ //Выводит сообщение в консоль
+        System.out.println(message);
+    }
+
+    public static String readString(){ //Считывает строку с консоли
+        while (true){
+            try{
+                return reader.readLine();
+            }
+            catch (IOException ex){
+                System.out.println("Произошла ошибка при попытке ввода текста.\nПопробуйте еще раз.");
+            }
+        }
+    }
+
+    public static int readInt(){ //Считывает число с консоли
+        while (true){
+            try{
+                return Integer.parseInt(readString());
+            }
+            catch (NumberFormatException ex){
+                System.out.println("Произошла ошибка при попытке ввода числа.\nПопробуйте еще раз.");
+            }
+        }
+    }
 }
