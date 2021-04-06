01a3a73f3c23afc7c4e849af496dec16c294636f
Aline Korshunova <aline.korshunova00@gmail.com>
2021-04-06 20:02:36 +0300

    Message.java class implemented, implements Serializable, has a type of message and data.

--- src/Message.java
+++ src/Message.java
@@ -1,3 +1,25 @@
+import java.io.Serializable;
+
 //Класс, отвечающий за пересылаемые сообщения
-public class Message {
+public class Message implements Serializable {
+    private final MessageType type; //Тип сообщения
+    private final String data; //Данные сообщения
+
+    public Message(MessageType type){
+        this.type = type;
+        this.data = null;
+    }
+
+    public Message(MessageType type, String data){
+        this.type = type;
+        this.data = data;
+    }
+
+    public MessageType getType() { //Получить тип сообщения
+        return type;
+    }
+
+    public String getData() { //Получить данные сообщения
+        return data;
+    }
 }
