3eba3ac3a3a4043b3128dc50f66bd16cb28d2e79
Aline Korshunova <aline.korshunova00@gmail.com>
2021-04-07 10:21:12 +0300

    Thread class Handler.java was created, added support for multiple server connections

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,34 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 //Основной класс сервера
 public class Server {
-    public static void main(String[] args) {
 
+    private static class Handler extends Thread{ //поток обработчик, в котором происходит обмен сообщениями с клиентом
+        private Socket socket;
+
+        public Handler(Socket socket){
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Введите порт сервера: ");
+        int port = ConsoleHelper.readInt(); //считываем порт
+        Socket clientSocket;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)){ //запускаем сервер
+            ConsoleHelper.writeMessage("Сервер запущен");
+
+            while(true){ //принимаем входящие сокетные соединения и запускаем новый поток Handler, если соединение установлено
+                clientSocket = serverSocket.accept(); //ждём, пока к нам не захотят присоединиться
+                new Handler(clientSocket).start(); //создаём обработчик соединения
+            }
+
+        } catch (Exception exception) {
+            ConsoleHelper.writeMessage("Произошла ошибка соединения");
+            ConsoleHelper.writeMessage(exception.getMessage());
+        }
     }
 }
