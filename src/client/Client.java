581d1dd57187aeeb478f1572ae4a0d98069c3064
Aline Korshunova <aline.korshunova00@gmail.com>
2021-04-08 23:30:57 +0300

    Implemented auxiliary methods of Client.java getServerAddress(), getServerPort(), getUserName() requests the user to enter the server address, port, user name, getSocketThread() - creates a new object of the SocketThread class, shouldSendTextFromConsole() - do we send the text entered in the console, sendTextMessage() - sending a message to the server

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,10 @@
 package client;
 import server.Connection;
+import server.ConsoleHelper;
+import server.Message;
+import server.MessageType;
+
+import java.io.IOException;
 
 public class Client {
     protected Connection connection;
@@ -7,5 +12,37 @@ public class Client {
 
     public class SocketThread extends Thread{ //Поток, устанавливающий сокетное соединение и читающий сообщения с сервера
 
+    }
+
+    protected String getServerAddress(){ //запрашивает ввод адреса сервера у пользователя
+        ConsoleHelper.writeMessage("Введите адрес сервера:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){ //запрашивает ввод порта сервера
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){ //запрашивает имя пользователя
+        ConsoleHelper.writeMessage("Введите имя пользователя:");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){ //в данной реализации клиента мы всегда отправляем текст введенный в консоль
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){ //создает новый объект класса SocketThread
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){ //отправка сообщения серверу
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException exception) {
+            ConsoleHelper.writeMessage("Не удалось отправить сообщение");
+            clientConnected = false;
+        }
     }
 }
