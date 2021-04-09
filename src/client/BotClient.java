7003091ccc665913ecbd518d3f6ec9d1a07132a8
Aline Korshunova <aline.korshunova00@gmail.com>
2021-04-09 20:41:28 +0300

    The BotSocketThread class is implemented, the processIncomingMessage() method processes incoming commands from users and sends a response. The bot is ready

--- src/client/BotClient.java
+++ src/client/BotClient.java
@@ -1,9 +1,62 @@
 package client;
 
+import server.ConsoleHelper;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class BotClient extends Client{
 
     public class BotSocketThread extends SocketThread{
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException { //отправка сообщения серверу
+            BotClient.this.sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
+        @Override
+        protected void processIncomingMessage(String message){ //Выводит текст message в консоль
+            ConsoleHelper.writeMessage(message); // Выводим текст сообщения в консоль
+
+            String[] split = message.split(": ");// Отделяем отправителя от текста сообщения
+            if (split.length != 2) return;
+
+            String messageWithoutUserName = split[1];
+
+            String format = null; // Подготавливаем формат для отправки даты согласно запросу
+            switch (messageWithoutUserName) {
+                case "дата":
+                    format = "d.MM.YYYY";
+                    break;
+                case "день":
+                    format = "d";
+                    break;
+                case "месяц":
+                    format = "MMMM";
+                    break;
+                case "год":
+                    format = "YYYY";
+                    break;
+                case "время":
+                    format = "H:mm:ss";
+                    break;
+                case "час":
+                    format = "H";
+                    break;
+                case "минуты":
+                    format = "m";
+                    break;
+                case "секунды":
+                    format = "s";
+                    break;
+            }
+
+            if (format != null) {
+                String answer = new SimpleDateFormat(format).format(Calendar.getInstance().getTime());
+                BotClient.this.sendTextMessage(String.format("Информация для %s: %s", split[0], answer));
+            }
+        }
     }
 
     @Override
